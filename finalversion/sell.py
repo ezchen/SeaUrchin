f3a35a10c1ea2995c331b7e82295d08dd64250bf
ezchen <echen2015@gmail.com>
2014-01-30 13:33:59 -0500

    made sell.py use one form

--- finalversion/sell.py
+++ finalversion/sell.py
@@ -4,35 +4,64 @@ import cgi, cgitb, os, lf
 cgitb.enable()
 
 #<---------------Everything by Muhamed Rahman------------->
-field=cgi.FieldStorage()
+from lf import field
 
-form='''<form class="form-horizontal action="sell.py" method="get" enctype="multipart/form-data">
-What would you like to sell?
-<div class="control-group">
-	<input type="text" name='item' placeholder="item name">
-	<select name="0">
-		<option value="All">All</option>
-		<option value="Home_and_Kitchen">Home and Kitchen</option>
-		<option value="Education">Education</option>
-		<option value="Electronics">Electronics</option>
-	</select>
-</div>
-	<textarea name="description" cols="25" rows="5">description</textarea>
-<div class="control-group">
-<select name="1">
-<option value="condition">condition</option>
-<option value="10">10</option>
-<option value="9">9</option>
-<option value="8">8</option>
-<option value="7">7</option>
-<option value="6">6</option>
-<option value="5">5</option>
-<option value="4">4 or lower</option>
-</select>
-</div>
-<input type="text" name="sb" placeholder="Starting Bid">
-<input type="submit" name="next" value="next"></form>
-'''
+form='''<div class="container">
+			<form class="form-horizontal" action="save_file.py" method="post" enctype="multipart/form-data">
+				<div class="control-group">
+					<label class="control-label" for="itemName"><b>Item Name</b></label>
+					<div class="controls">
+					<input class="input-large" type="text" name="item" id="itemName" placeholder="item name">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="category"><b>Category</b></label>
+					<div class="controls">
+					<select id="category" name="0" class="input-large">
+						<option value="All">All</option>
+						<option value="Home_and_Kitchen">Home and Kitchen</option>
+						<option value="Education">Education</option>
+						<option value="Electronics">Electronics</option>
+					</select>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="description"><b>Description</b></label>
+					<div class="controls">
+					<textarea class="input-large" name="description" id="description" cols="25" rows="5"></textarea>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="condition"><b>Condition</b></label>
+					<div class="controls">
+					<select name="1" id="condition">
+					<option value="10">10</option>
+					<option value="9">9</option>
+					<option value="8">8</option>
+					<option value="7">7</option>
+					<option value="6">6</option>
+					<option value="5">5</option>
+					<option value="4">4 or lower</option>
+					</select>
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="bid"><b>Starting Bid</b></label>
+					<div class="controls">
+					<input type="text" name="sb" placeholder="Enter Bid">
+					</div>
+				</div>
+				<div class="control-group">
+					<label class="control-label" for="fileinput"><b>Upload an Image</b></label>
+					<div class="controls">
+					<input type="file" id="fileinput" name="file">
+					</div>
+				</div>
+				<div class="controls">
+					<button type="submit" class="btn btn-default" name="next" value="next">Submit</button>
+				</div>
+			</form>
+</div>'''
 
 page='''
 <!DOCTYPE html>
@@ -76,19 +105,5 @@ def sell():
 
 if len(field)<1:
     page+=form
-if 'next' in field.keys():
-    page+='''<form enctype="multipart/form-data" action="save_file.py" method="post" >
-<p>Image Files ONLY: <input type="file" name="file"></p>
-<input type="hidden" name="item" value="'''+ str(field.getvalue('item'))+'''">
-<input type="hidden" name="0" value="'''
-    page+=str(field.getvalue('0'))+'''">
-<input type="hidden" name="description" value="'''
-    page += str(field.getvalue('description'))+'''">
-<input type="hidden" name="sb" value="'''
-    page += str(field.getvalue('sb'))+'''">
-<input type="hidden" name="1" value="'''
-    page+=str(field.getvalue('1'))+'''">
-<input type="submit" value="Upload">
-</form>'''
 
 print page + '</body></html>' 
